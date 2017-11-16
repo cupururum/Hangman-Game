840f3b56e7e0b56e116d01fa4b90770701832d82
cupururum <cupururum@Vogon-2.local>
2017-11-16 09:35:30 -0800

    need to reset lettersAlreadyGuessed

--- game_draft.js
+++ game_draft.js
@@ -1,27 +1,20 @@
-var words = ["harry potter", "muglle", "albus dumbledore"];
-var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
-      'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
-      't', 'u', 'v', 'w', 'x', 'y', 'z'];
-var pickUpWord;
-var pickUpWordArray = [];
-var beginGame;
-var userGuess;
-var rigthGuess = 0;
-var wrongGuess = 0;
-var numberOfTries = 8;
-var lettersWordLeft;
 var loses = 0;
 var wins = 0;
-var pickUpWordSlots = [];
-var alreadyGuessArray = [];
-var chooseAlph = document.getElementById('chooseAlph');
-var alreadyGuessed = document.getElementById('alreadyGuessed');
 var currentWordP = document.getElementById('currentWord');
-words = words.map(word => word.toUpperCase());
-alphabet = alphabet.map(letter => letter.toUpperCase());
+var lettersWordLeft;
 
-
-function game(word) {
+function game(word, slots) {
+  var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
+        'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
+        't', 'u', 'v', 'w', 'x', 'y', 'z'];
+  alphabet = alphabet.map(letter => letter.toUpperCase());
+  var userGuess;
+  var rigthGuess = 0;
+  var wrongGuess = 0;
+  var numberOfTries = 8;
+  var alreadyGuessArray = [];
+  var alreadyGuessed = document.getElementById('alreadyGuessed');
+  var chooseFromAlphabet = document.getElementById('chooseAlph');
       document.onkeypress = function(event) {
         userGuess = event.key;
         userGuess = userGuess.toUpperCase();
@@ -33,25 +26,25 @@ function game(word) {
             //publish the guess in the document
             alreadyGuessed.textContent = 'Letters already guessed: ' + alreadyGuessArray.join(' ');
             // Second, I begin to search a character in the word
-           if (pickUpWord.indexOf(userGuess) > -1) {
+           if (word.indexOf(userGuess) > -1) {
              //i need to check if the letter represented in my word seversl times
-             var currentLetterGuessIndex = pickUpWord.indexOf(userGuess);
+             var currentLetterGuessIndex = word.indexOf(userGuess);
              var indicesOfLetter = [];
              while (currentLetterGuessIndex != -1) {
                indicesOfLetter.push(currentLetterGuessIndex);
-               currentLetterGuessIndex = pickUpWord.indexOf(userGuess, currentLetterGuessIndex + 1);
+               currentLetterGuessIndex = word.indexOf(userGuess, currentLetterGuessIndex + 1);
                console.log(indicesOfLetter);
                // for each index I change the underscore for the letter
                for ( i = 0; i < indicesOfLetter.length; i++) {
-                 pickUpWordSlots[indicesOfLetter[i]] = userGuess;
-                 currentWordP.textContent = "Current word: " + pickUpWordSlots.join(' ');
+                 slots[indicesOfLetter[i]] = userGuess;
+                 currentWordP.textContent = "Current word: " + slots.join(' ');
 
                }//closing for loop after I changed all letters in the word
                lettersWordLeft--;
                console.log('letter left ' + lettersWordLeft );
                if (lettersWordLeft == 0){
                  wins++;
-                 document.getElementById('wins').textContent = 'wins: ' + loses;
+                 document.getElementById('wins').textContent = 'Wins: ' + wins;
                  startGame();
                }
              }//closing the while loop for the all letters in the word
@@ -73,7 +66,7 @@ function game(word) {
          } //closing you cannot use the same letter twice!!
 
         } else {
-          chooseAlph.textContent = 'You need to choose a letter from alphabet: ' + alphabet.join(" ");
+          chooseFromAlphabet.textContent = 'You need to choose a letter from alphabet: ' + alphabet.join(" ");
         } //closing if letter is not from the alphabet you will see this
       }//closing the document.onkeypress evnt function for the game
 
@@ -82,6 +75,10 @@ function game(word) {
 
 
 function startGame() {
+  var words = ["harry potter", "muglle", "albus dumbledore"];
+  var pickUpWord;
+  var pickUpWordSlots = [];
+  words = words.map(word => word.toUpperCase());
   pickUpWord = words[Math.floor(Math.random() * words.length)]; // computer choose a word
   for ( j = 0; j < pickUpWord.length; j++) {
      pickUpWordSlots.push('_ ');
@@ -89,7 +86,10 @@ function startGame() {
   currentWordP.textContent = "Current word: " + pickUpWordSlots.join(' ');// publish underscore word into the doc
       // after I published underscore word I start guessing
   lettersWordLeft = pickUpWord.replace(/\s/g, "").length;
-  game(pickUpWord);
+
+  //choose from alphabet
+
+  game(pickUpWord, pickUpWordSlots);
 }
 
 document.onkeypress = function(e) {
